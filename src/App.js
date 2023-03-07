24d96a76f925efc3193c22c6df54e55f8698c733
EnterOrion <orionwesson@gmail.com>
2023-03-07 05:04:01 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles/style.scss";
+
+function App() {
+  return (
+    <div className="main">
+        <p>
+         What did you dream of last night...
+        </p>
+        <input />
+    </div>
+  );
+}
+
+export default App;
