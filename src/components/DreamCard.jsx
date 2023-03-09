79c931543b6708b2948daeff68b0bea182673437
EnterOrion <orionwesson@gmail.com>
2023-03-10 06:33:33 +0800

    Home page functionality

--- src/components/DreamCard.jsx
+++ src/components/DreamCard.jsx
@@ -1,7 +1,10 @@
-import React, {useContext} from "react";
+import React, {useContext, useState, useRef, useEffect} from "react";
 import DreamContext from "../contexts/DreamContext";
 
 const DreamCard = (props) => {
+    const [overflowActive, setOverflowActive] = useState(false);
+    const [readMore, setReadMore] = useState(false);
+    
     const dreamContext = useContext(DreamContext);
     const { removeDream} = dreamContext;
     const handleDelete = (e) => {
@@ -9,14 +12,45 @@ const DreamCard = (props) => {
      
     }
 
+    const textRef = useRef();
+    
+    function isOverflowActive(event) {
+        return event.offsetHeight < event.scrollHeight || event.offsetWidth < event.scrollWidth;
+    }
+
+    useEffect(() => {
+        if (isOverflowActive(textRef.current)) {
+            setOverflowActive(true);
+            return;
+        }
+    
+        setOverflowActive(false);
+    }, []);
+
+    const readMoreHandler = () => {
+        setReadMore(true);
+    }
+   
+    const closeHandler = () => {
+        setReadMore(false);
+    }
+
     return (
-    <div className="card">
-     <div className="ribbon">
-    <span className="ribbon3">{props.date}</span>
-    </div>
-    <p>{props.dream}</p>
-    <button className="read-more">Read more</button>
-    <button className="read-more" value={props.id} onClick={handleDelete}>Delete Entry</button>
+    
+    <div>
+        <div className="card">
+         <div className="ribbon">
+        <span className="ribbon3">{props.date}</span>
+        </div>
+        <p ref={textRef}>{props.dream}</p>
+        {overflowActive && <button className="read-more" onClick={readMoreHandler}>Read more</button>}
+        <button className="delete-entry" value={props.id} onClick={handleDelete}>Delete Entry</button>
+        </div>
+        {readMore && <div className="dream-detail">
+            <p onClick={closeHandler} className="close-button">X</p>
+            <h2 className="date-header">Dream on {props.date}</h2>
+            {props.dream}
+        </div>}
     </div>
     )
 }
